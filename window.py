c30a67c2d5f809bb4c9cbf296515e0054e019bd4
Adithyan K V <29067795+adithyankv@users.noreply.github.com>
2023-02-17 18:09:04 +0530

    Redesign UI
    - Redesign to a simpler single window UI.
    - Gives user better visual feedback

--- window.py
+++ window.py
@@ -1,36 +1,87 @@
-from PySide6.QtCore import Slot
-from PySide6.QtWidgets import QMainWindow, QStackedWidget
+import mimetypes
+import re
 
-from audio_selection_view import AudioSelectionView
-from offset_selection_view import OffsetSelectionView
-from timestamp_selection_view import TimestampSelectionView
+from PySide6.QtCore import Qt, Slot
+from PySide6.QtWidgets import (QFileDialog, QGridLayout, QLabel, QLineEdit,
+                               QMainWindow, QMessageBox, QPushButton,
+                               QStackedWidget, QVBoxLayout, QWidget)
 
 
 class MainWindow(QMainWindow):
     def __init__(self) -> None:
         super().__init__()
+        self.create_layout()
+
+        self.audio_browse_button.clicked.connect(self.on_audio_browse_clicked)
+        self.timestamp_browse_button.clicked.connect(self.on_timestamp_browse_clicked)
+
+    def create_layout(self) -> None:
         self.setWindowTitle("App")
-        self.resize(250, 200)
+        self.resize(600, 200)
 
-        self.stack = QStackedWidget()
-        self.setCentralWidget(self.stack)
+        container = QWidget()
+        self.setCentralWidget(container)
 
-        self.offset_selection = OffsetSelectionView()
-        self.audio_selection = AudioSelectionView()
-        self.timestamp_selection = TimestampSelectionView()
-        self.stack.addWidget(self.audio_selection)
-        self.stack.addWidget(self.timestamp_selection)
-        self.stack.addWidget(self.offset_selection)
+        grid_layout = QGridLayout()
 
-        self.audio_selection.audio_file_selected.connect(self.on_audio_selected)
-        self.timestamp_selection.timestamps_file_selected.connect(
-            self.on_timestamps_selected
-        )
+        audio_label = QLabel("Audio file")
+        self.audio_entry = QLineEdit()
+        self.audio_browse_button = QPushButton("Browse")
+
+        timestamp_label = QLabel("Timestamp file")
+        self.timestamp_entry = QLineEdit()
+        self.timestamp_browse_button = QPushButton("Browse")
+
+        offset_label = QLabel("Offset")
+        self.offset_entry = QLineEdit()
+        self.offset_entry.setInputMask("09:09:09.999")
+        self.offset_entry.setText("00:00:00.000")
+
+        grid_layout.addWidget(audio_label, 0, 0)
+        grid_layout.addWidget(self.audio_entry, 0, 1)
+        grid_layout.addWidget(self.audio_browse_button, 0, 2)
+        grid_layout.addWidget(timestamp_label, 1, 0)
+        grid_layout.addWidget(self.timestamp_entry, 1, 1)
+        grid_layout.addWidget(self.timestamp_browse_button, 1, 2)
+        grid_layout.addWidget(offset_label, 2, 0)
+        grid_layout.addWidget(self.offset_entry, 2, 1)
+
+        cut_button = QPushButton("Cut audio clip")
+        cut_button.setDefault(True)
+
+        vbox = QVBoxLayout()
+        vbox.addStretch()
+        vbox.addLayout(grid_layout)
+        vbox.addStretch()
+        vbox.addWidget(cut_button)
+        vbox.setAlignment(Qt.AlignmentFlag.AlignCenter)
+
+        container.setLayout(vbox)
 
     @Slot()
-    def on_audio_selected(self) -> None:
-        self.stack.setCurrentWidget(self.timestamp_selection)
+    def on_audio_browse_clicked(self) -> None:
+        dialog = QFileDialog()
+        dialog.setFileMode(QFileDialog.ExistingFile)
+        if dialog.exec():
+            urls = dialog.selectedFiles()
+            url = urls[0]
+            # assume it is not audio if mimetype can't be guessed.
+            mimetype = mimetypes.guess_type(url)[0] or "not audio"
+            is_audio = bool(re.match(r"audio/*", mimetype))
+            if not is_audio:
+                error_box = QMessageBox()
+                error_box.critical(
+                    self, "File type error", "Please select an audio file"
+                )
+                return
+            self.audio_entry.setText(url)
 
     @Slot()
-    def on_timestamps_selected(self) -> None:
-        self.stack.setCurrentWidget(self.offset_selection)
+    def on_timestamp_browse_clicked(self) -> None:
+        dialog = QFileDialog()
+        dialog.setFileMode(QFileDialog.ExistingFile)
+        dialog.setMimeTypeFilters(["application/json"])
+        if dialog.exec():
+            urls = dialog.selectedFiles()
+            url = urls[0]
+            self.timestamp_entry.setText(url)
